e1fe964a0416f6daa02e6dc2891fa7089d7773e6
Nathanael Cruz <nathanael.cruz.fd@gmail.com>
2020-08-14 20:23:59 -0300

    Add Animation mobile

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from 'jquery'
 import ImageForArticle from '../../assets/images/drawers.jpg'
 import IconFacebook from '../../assets/images/icon-facebook.svg'
 import IconTwitter from '../../assets/images/icon-twitter.svg'
@@ -6,6 +7,30 @@ import IconPinterest from '../../assets/images/icon-pinterest.svg'
 import AvatarPerfil from '../../assets/images/avatar-michelle.jpg'
 
 import './style.css'
+
+const animationButton = () => {
+    let buttonForAnimate = $(".card-article-info-perfil-btn")
+    buttonForAnimate.toggleClass('active')
+    
+}
+const animationSocialMedia = () => {
+
+    let cardArticlePerfilSocialMedia = $(".card-article-info-perfil-social")
+    cardArticlePerfilSocialMedia.toggleClass('active-social')
+
+    if(cardArticlePerfilSocialMedia.hasClass('active-social')){
+        $(".card-article-info-perfil-social").animate({
+            width: '100%',
+            'padding-left': '2rem'
+          }, 1000, "linear")
+    } else{
+        $(".card-article-info-perfil-social").animate({
+            width: '0%',
+            'padding-left': '0rem'
+          }, 1000, "linear")
+    }
+    animationButton()
+}
 
 function Card() {
     return (
@@ -45,7 +70,7 @@ function Card() {
                             <img src={IconPinterest} alt="Ícone Pinterest"/>
                         </section>
                     </section>
-                    <button className="card-article-info-perfil-btn">
+                    <button className="card-article-info-perfil-btn" onClick={animationSocialMedia}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="15" height="13"><path fill="#6E8098" d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"/></svg>
                     </button>
                 </section>
